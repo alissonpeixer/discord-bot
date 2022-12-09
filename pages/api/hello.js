d0f01bc96ad7013c5c91e942b1dd876ebb3f9a98
Alisson Peixer <alissonpeixer4@gmail.com>
2022-12-09 17:06:10 -0300

    feat: fixed api

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,80 +1,83 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-const { PlayerManager } = require("discord-player-plus");
-const { Client, GatewayIntentBits, Partials } = require("discord.js")
-
-const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds,
-    GatewayIntentBits.MessageContent,
-    GatewayIntentBits.GuildMembers,
-    GatewayIntentBits.GuildMessages,
-    GatewayIntentBits.GuildVoiceStates
-  ],
-  presence: [
-    Partials.Channel,
-    Partials.Message
-  ]
-});
+export default function handler(req, res) {
 
 
-client.once('ready', data => {
-  global.guildsName = []
+  // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-  client.guilds.cache.forEach(guild => { guildsName.push(guild) });
-  console.log(`Bot online no servers, ${guildsName}`)
-})
+  const { PlayerManager } = require("discord-player-plus");
+  const { Client, GatewayIntentBits, Partials } = require("discord.js")
+
+  const client = new Client({
+    intents: [
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.MessageContent,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildVoiceStates
+    ],
+    presence: [
+      Partials.Channel,
+      Partials.Message
+    ]
+  });
 
 
+  client.once('ready', data => {
+    global.guildsName = []
 
-
-
-client.on("messageCreate", async (interaction) => {
-
-  if (interaction.content.split(' ')[0] === `play`) {
-    const song = interaction.content.split('play')[1]
-    console.log(song)
+    client.guilds.cache.forEach(guild => { guildsName.push(guild) });
+    console.log(`Bot online no servers, ${guildsName}`)
+  })
 
 
 
-    const playerManager = new PlayerManager();
-    const player = playerManager.get(interaction.guild.id);
+
+
+  client.on("messageCreate", async (interaction) => {
+
+    if (interaction.content.split(' ')[0] === `play`) {
+      const song = interaction.content.split('play')[1]
+      console.log(song)
 
 
 
-    interaction.reply(':fire:  Buscando, isso pode demorar....').then(msg => setTimeout(() => { msg.delete() }, 2000));
-
-    const searchResults = await player.search(song, {
-      source: "spotify",
-
-    });
-
-    console.log(searchResults)
+      const playerManager = new PlayerManager();
+      const player = playerManager.get(interaction.guild.id);
 
 
 
-    if (searchResults.length && searchResults[0].tracks.length) {
+      interaction.reply(':fire:  Buscando, isso pode demorar....').then(msg => setTimeout(() => { msg.delete() }, 2000));
 
-      await player.play({
-        channel: interaction.member.voice.channel,
-        tracks: searchResults[0].tracks,
+      const searchResults = await player.search(song, {
+        source: "spotify",
+
       });
 
+      console.log(searchResults)
 
 
-      return await interaction.reply({ content: `:loud_sound:  | Tocando agora !` });
+
+      if (searchResults.length && searchResults[0].tracks.length) {
+
+        await player.play({
+          channel: interaction.member.voice.channel,
+          tracks: searchResults[0].tracks,
+        });
+
+
+
+        return await interaction.reply({ content: `:loud_sound:  | Tocando agora !` });
+      }
+
+
     }
 
 
-  }
 
 
+  });
 
 
-});
-
-
-client.login(process.env.TOKEN);
-export default function handler(req, res) {
+  client.login(process.env.TOKEN);
   res.status(200).json({ name: 'John Doe' })
 }
